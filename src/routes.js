bb54dc0c24e4591a33f1fe71ab985d0b5a3e9c06
dudu <137736192+duduxqx@users.noreply.github.com>
2023-08-09 13:12:11 -0300

    Delete routes.js

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const CreateProductController_1 = require("./controllers/CreateProductController");
-const router = (0, express_1.Router)();
-exports.router = router;
-const createProduct = new CreateProductController_1.CreateProductController();
-router.post("/product", createProduct.handle);
